c16e87b5db8b3f7c1c21bfba538cf25ba68cf745
Ubuntu <sasamura@rails-study-vm.yrnpgrqpbngurhavrzim2lpagc.lx.internal.cloudapp.net>
2019-09-30 07:44:15 +0000

    add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_060730) do
+ActiveRecord::Schema.define(version: 2019_09_30_071352) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", default: "", null: false
